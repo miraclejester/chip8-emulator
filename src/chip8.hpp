caf00404ddbec427336bf77903aa82c9968a8b04
miraclejester <miraclejester413@gmail.com>
2026-09-06 01:24:45 -0400

    Initial commit

---
+++ src/chip8.hpp
@@ -0,0 +1,22 @@
+﻿class Chip8
+{
+public:
+    void loadRom(const std::string& path);
+    void cycle(); // fetch, decode, execute
+    void tickTimers(); // Call at 60hz
+    
+    const std::array<uint8_t, 64*32>& display() const;
+    void setKey(uint8_t key, bool pressed);
+    bool drawFlag = false;
+    
+private:
+    std::array<uint8_t, 4096> memory{};
+    std::array<uint8_t, 16> V{}; // registers V0 .. VF
+    uint16_t I = 0;
+    uint16_t pc = 0x200;
+    std::array<uint16_t, 16> stack{};
+    uint8_t sp = 0;
+    uint8_t delayTimer = 0, soundTimer = 0;
+    std::array<uint8_t, 64*32> gfx{};
+    std::array <bool, 16> keys{};
+};
---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+﻿#include <SDL2/SDL.h>
+#include <cstdio>
+
+int main(int argc, char* argv[])
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		std::fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	SDL_Window* window = SDL_CreateWindow(
+		"Chip-8",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		64*10, 32*10,
+		SDL_WINDOW_SHOWN
+	);
+
+	if (!window) {
+		std::fprintf(stderr, "CreateWindow failed: %s\n", SDL_GetError());
+		SDL_Quit();
+		return 1;	
+	}
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (!renderer) {
+		std::fprintf(stderr, "CreateRenderer failed: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+	}
+
+	bool running = true;
+	while (running) {
+		SDL_Event e;
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT) running = false;
+			if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) running = false;
+		}
+		SDL_SetRenderDrawColor(renderer, 20, 20, 30, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
